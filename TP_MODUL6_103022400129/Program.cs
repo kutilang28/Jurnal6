19f706cbe7cf891c0ca7494bb353d8668d74dad0
Fidel Gandewo <fidelgandewo@gmail.com>
2026-04-05 15:38:31 +0700

    menambahkan implementasi design by contrac

--- TP_MODUL6_103022400129/Program.cs
+++ TP_MODUL6_103022400129/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,20 +15,39 @@ namespace TP_MODUL6_103022400129
 
         public SayaMusicTrack(string title)
         {
+            Debug.Assert(title != null, "Judul track tidak boleh null.");
+            Debug.Assert(title.Length <= 100, "Judul track maksimal 100 karakter.");
+
             this.title = title;
             Random rand = new Random();
-            this.id = rand.Next(10000, 99999); 
+            this.id = rand.Next(10000, 99999);
             this.playCount = 0;
         }
 
         public void IncreasePlayCount(int count)
         {
-            this.playCount += count;
+            Debug.Assert(count <= 10000000, "Input penambahan maksimal 10.000.000.");
+
+            try
+            {
+                checked // Memastikan overflow melempar exception, bukan silent error
+                {
+                    this.playCount += count;
+                }
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Error: Overflow.");
+            }
         }
 
         public void PrintTrackDetails()
         {
-            Console.WriteLine($"ID: {this.id}, Title: {this.title}, PlayCount: {this.playCount}");
+            Console.WriteLine("----------------------------------");
+            Console.WriteLine($"ID         : {this.id}");
+            Console.WriteLine($"Title      : {this.title}");
+            Console.WriteLine($"Play Count : {this.playCount}");
+            Console.WriteLine("----------------------------------");
         }
     }
     class Program
